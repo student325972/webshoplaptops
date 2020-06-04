b234cbdc7a82d123de5af670b54ce85b66955a8b
student325972 <56292287+student325972@users.noreply.github.com>
2020-06-04 11:06:14 +0200

    test

---
+++ layout-basic/footer.php
@@ -0,0 +1,46 @@
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <h5 class="footer-text"></h5>
+        <p>
+          
+        </p>
+      </div>
+      <div class="col-12 col-md-2">
+        <h5 class="footer-text"></h5>
+        <p><a href="./index.php?content=home"></a></p>
+        <p><a href="./index.php?content=informatie"></a></p>
+        <p><a href="./index.php?content=shop"></a></p>
+        <p><a href="./index.php?content=contact"></a></p>
+      </div>
+      <div class="col-12 col-md-2">
+        <h5 class="footer-text"></h5>
+        <p><a href="#"></a></p>
+        <p><a href="index.php?content=informatie#locatie"></a></p>
+        <p><a href="index.php?content=inloggen"></a></p>
+      </div>
+      <div class="col-12 col-md-2">
+        <h5 class="footer-text">Contactinformatie</h5>
+        <p></p>
+        <p>06 - 33 44 55 60</p>
+        <p>info@laptops.com</p>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid copyright-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-7 col-lg-8">
+          <h5 class="copyright-text"></h5>
+        </div>
+        <div class="col-sm-12 col-md-5 col-lg-4 footer-social">
+          <a href="https://www.facebook.com/mboutrechtalgemeen/" target="_blank"><i class="fab fa-facebook"></i></a>
+          <a href="https://twitter.com/mboutrecht" target="_blank"><i class="fab fa-twitter"></i></a>
+          <a href="https://www.youtube.com/channel/UCxarjRC3rXOxfMZ0eksWQgQ" target="_blank"><i class="fab fa-youtube"></i></a>
+          <a href="https://nl.linkedin.com/company/mbo-utrecht" target="_blank"><i class="fab fa-linkedin"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ layout-basic/navbar.php
@@ -0,0 +1,35 @@
+<?php $content = (isset($_GET['content']) ? $_GET['content'] : false); ?>
+<div class="upper-navbar">
+  <div class="container-fluid upper-navbar-content">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container">
+    <div class="row">
+      <a class="navbar-brand" href="index.php?content=home"><img src="./img/homelogo.png" alt="Homepage Logo" class="img-fluid"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="<?php if ($content == 'home') echo 'active' ?>">
+            <a href="./index.php?content=home">Home</a></li>
+          <li class="<?php if ($content == 'informatie') echo 'active' ?>">
+            <a href="index.php?content=informatie">Informatie</a></li>
+          <li class="<?php if ($content == 'shop') echo 'active' ?>">
+            <a href="index.php?content=shop">Laptops</a></li>
+          <li class="<?php if ($content == 'contact') echo 'active' ?>">
+            <a href="index.php?content=contact">Contact</a></li>
+          <li class="<?php if ($content == 'faq') echo 'active' ?>">
+            <a href="index.php?content=faq">FAQ</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
